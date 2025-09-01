c8a4ba0d291e05567cbd22c6387386ba15183188
Jorge Cortés <jcortesanguita@gmail.com>
2025-09-01 13:29:21 +0200

    Refactor, extract function check_valid_board_size and constant

--- src/sudoku_validator/main.py
+++ src/sudoku_validator/main.py
@@ -1,3 +1,6 @@
+BOARD_SIZE = 2
+
+
 def is_valid_sudoku(board: list[list[int]]) -> bool:
         """
         Validates if a given Sudoku board is valid, according to Sudoku rules.
@@ -14,10 +17,12 @@ def is_valid_sudoku(board: list[list[int]]) -> bool:
             ValueError: If the board dimensions are not valid (2x2) or contain invalid value types.
         """
 
-        # Check if the board is 2x2
-        size = 2
-
-        if size != len(board) or any(len(row) != size for row in board):
-            raise ValueError("Board must be {}x{}.".format(size, size))
+        # Check if the board is proper size (2x2)
+        check_valid_board_size(board, BOARD_SIZE)
 
         return None
+
+def check_valid_board_size(board: list[list[int]], size: int) -> bool:
+    if size != len(board) or any(len(row) != size for row in board):
+        raise ValueError("Board must be {}x{}.".format(size, size))
+    return True
