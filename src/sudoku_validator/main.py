65465a910b5b28a352d4a39dfe800f1fbe67d3b0
Jorge Cortés <jcortesanguita@gmail.com>
2025-09-01 15:54:54 +0200

    Resolve 9x9 sudoku boards, direct adaptation from 2x2

--- src/sudoku_validator/main.py
+++ src/sudoku_validator/main.py
@@ -1,23 +1,21 @@
-BOARD_SIZE = 2
-
+BOARD_SIZE: int = 9
 
 def is_valid_sudoku(board: list[list[int]]) -> bool:
         """
         Validates if a given Sudoku board is valid, according to Sudoku rules.
 
-
         Args:
-            board (list[list[int]]): A 2x2 nested list representing the Sudoku board.
-                                    Each cell contains integers from 1-4.
+            board (list[list[int]]): A 9x9 nested list representing the Sudoku board.
+                                    Each cell contains integers from 1-9.
 
         Returns:
             bool: True if the board is valid, False otherwise.
 
         Raises:
-            ValueError: If the board dimensions are not valid (2x2) or contain invalid value types.
+            ValueError: If the board dimensions are not valid (9x9) or contain invalid value types.
         """
 
-        # Check if the board is proper size (2x2)
+        # Check if the board is proper size (9x9)
         check_valid_board_size(board, BOARD_SIZE)
 
         # Check if all values are integers between 1 and BOARD_SIZE^2
@@ -41,12 +39,12 @@ def check_valid_board_size(board: list[list[int]], size: int) -> bool:
     return True
 
 def check_valid_values(board: list[list[int]]) -> bool:
-    valid_values = range(1, (BOARD_SIZE**2)+1)  # For board size of 2x2, valid values are 1, 2, 3, 4
+    valid_values = range(1, BOARD_SIZE+1)  # For board size of 9x9, valid values are 1 to 9
 
     for row in board:
         for value in row:
             if not isinstance(value, int) or value not in valid_values:
-                raise ValueError("Board values must be integers between 1 and {}.".format(BOARD_SIZE**2))
+                raise ValueError("Board values must be integers between 1 and {}, value: {}.".format(BOARD_SIZE, value))
     return True
 
 def check_unique_values_in_rows(board: list[list[int]]) -> bool:
